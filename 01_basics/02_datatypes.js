4b0329c3c42485d0ddb95c8e44a25a26f48657a2
Nitin123450 <nynitinyadav45@gmail.com>
2025-04-15 09:00:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version 
+
+
+// alert(3+3) // we are using nodejs not console
+ 
+
+  // types of datatypes : primitive 
+let number = 1234 // number (bigint => used when number is too big)
+let name = "nitin" // string , use double quotes 
+let b = false // boolean (true/false) 
+ let state // undefined (when value is not defined)
+let we = null // null is a standalone value means empty (null is type of object )
+              // symbol (used for uniqueness)
+
+  console.log(typeof name)            
+
+
