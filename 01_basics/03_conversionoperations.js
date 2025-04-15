d30763e929fe8fccbc1dc5f03e5d84e995310506
Nitin123450 <nynitinyadav45@gmail.com>
2025-04-15 10:20:31 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+console.log(typeof(score))
+
+let scoreInNumber = Number(score)  // converting string to number (use datatypes in capital)
+console.log(scoreInNumber)
+console.log(typeof(scoreInNumber))
+
+//  "33" => 33
+// "33abd" => NaN
+// true => 1 and false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+let isLoggedOut = ""
+let booleanIsLoggedOut = Boolean(isLoggedOut)
+console.log(booleanIsLoggedOut);
+
+ // 1 => true and 0 => false 
+ // "" => false and "nitin" => true
+
+ let number = 33
+ let stringNumber = String(number)
+ console.log(stringNumber)
+ console.log(typeof(stringNumber))  // string
+
