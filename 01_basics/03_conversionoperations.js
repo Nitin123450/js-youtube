809dea11d2ee10d90bce9f57a3188fe9ad6fe25c
Nitin123450 <nynitinyadav45@gmail.com>
2025-04-15 11:08:32 +0000

    Datatype Conversion  & operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -27,3 +27,31 @@ console.log(booleanIsLoggedOut);
  console.log(stringNumber)
  console.log(typeof(stringNumber))  // string
 
+
+
+ // ******************************operations*****************************
+
+ // Arithmetic operations as we all knew
+
+// console.log(2 + 2)  // 4 (Addition)
+// console.log(2 - 2)  // 0  (subtraction)
+// console.log(2 * 2)  // 4  (multiplication)
+// console.log(2 ** 2) // 4 , power (2 raise to power 2)
+// console.log(2 % 2)   // 0 (modulous operator)
+// console.log(2 / 2)    // 1  (divide)
+
+
+// prefix and postfix
+ 
+// postfix : first value is used then increment or decrement 
+let a = 2
+let b = ++a
+console.log(a , b) // 3 , 2 
+
+// prefix : first value is increment or decrement then used 
+let x = 3
+let y = x++
+console.log(x , y) // 4 , 4
+
+
+
