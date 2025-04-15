0b2748f8e3ddbbc9b592956a133651d20eb1f8a7
Nitin123450 <nynitinyadav45@gmail.com>
2025-04-15 13:12:52 +0000

    Stack & Heap memory

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -5,6 +5,16 @@
 // Note : JavaScript is a dynamically typed language. 
 //        This means that the type of a variable is not checked at compile time
 
+  /* Description : //datatypes are based on memory allocation
+Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+JavaScript is a dynamically typed language. This means that you don't need to specify
+ the data type of a variable when you declare it.
+*/
+
+
 // Examples
 let score = 100
 let name = "nitin"
@@ -46,3 +56,35 @@ console.log(typeof id)  // symbol
 console.log(typeof myArr)   // object
 console.log(typeof myObj)  //object
 console.log(typeof myFunction)  // function-object
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Memory management
+
+// stack (Primitive Datatype)     Heap (Non-primitive )
+
+let name1 = "nitin"
+let name2 = name1 
+console.log(name1)  // nitin
+console.log(name2)  // nitin  
+name2 = "Nitin2"
+console.log(name1 )  // nitin
+console.log(name2)  // Nitin2  , because copy of value is used not the reference
+
+let obj1 = {
+    email : "nitin@google.com",
+    upi :  "nitin@ybl"
+}
+
+let obj2 = obj1   
+
+console.log(obj1)
+console.log(obj2)
+
+ obj1.email = "nitin@gmail.com" 
+
+ console.log(obj1)  // value is changed as the reference of the object is used (eamil: "nitin@gmail.com")
+ console.log(obj2)
+
+
+
+
